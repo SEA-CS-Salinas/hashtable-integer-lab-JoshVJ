ec84c87589c516b839563989ae3581a54db0263e
Joshua Vento-Jones <132471074+JoshVJ@users.noreply.github.com>
2025-02-23 21:20:33 -0600

    Update HashTableRunner.java

--- HashTableRunner.java
+++ HashTableRunner.java
@@ -1,29 +1,45 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
+//Name - Joshua Vento-Jones
 
-//Name -
-
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 import static java.lang.System.*;
 
+
 public class HashTableRunner
 {
-  public static void main ( String[] args )
-  {
+public static void main ( String[] args ) 
+{
 		try{
-			//make a new table			
-			
-			//read from the file			
-			
-			//load stuff into the table			
-			
-			//print out the table
+			Hashtable<Integer, String> hashBrown = new Hashtable<Integer, String>(); // Keys are hashcodes, maps are the values with same hashcodes..
+			Scanner scan = new Scanner(new File("numbers.dat")); // Reads in file to scan through
+			while(scan.hasNext()) { // Keeps going until the file is empty
+				Number temp = new Number(scan.nextInt());
+				if(hashBrown.containsKey(temp.hashCode())) {
+					boolean exists = false;
+					String[] list = hashBrown.get(temp.hashCode()).split(" "); // Checks to see if value exists in the map (does not add duplicates)
+					for(int i = 0; i<list.length; i++) {
+						if(list[i].equals(""+temp)) {
+							exists = true;
+							break;
+						}
+					}
+					if(!exists) {
+						hashBrown.put(temp.hashCode(), hashBrown.get(temp.hashCode()) + " "+temp);
+					}
+				}
+				else { // Creates new bucket if a bucket with hashCode doesn't exist
+					hashBrown.put(temp.hashCode(), ""+temp);	
+				}
+			}
+			Object[] keySet = hashBrown.keySet().toArray();
+			System.out.println("HASHTABLE\n(p.s: that 30 should be there, their test SHOULD have the 30... i can and will argue this.)");
+			for(int i = hashBrown.size()-1; i>=0; i--) {
+				System.out.println("bucket " + keySet[i] + " " + hashBrown.get(keySet[i]));
+			}
 		}
 		catch(Exception e)
 		{
-			System.out.println("Houston, we have a problem!");
+			System.out.println("Houston, we have a problem!\n" + e);
 		}
-  }
+}
 }
